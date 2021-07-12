d73e4ec9f184bce941176845ba5c71c761db4ebc
Johannes Brauer <brauer@nordakademie.de>
2021-07-12 13:33:40 +0200

    prod build dep gelöscht

--- target/public/cljs-out/dep/clojure/test/check/impl.js
+++
@@ -1,7 +0,0 @@
-// Compiled by ClojureScript 1.10.773 {:static-fns true, :optimize-constants true}
-goog.provide('clojure.test.check.impl');
-goog.require('cljs.core');
-goog.require('cljs.core.constants');
-clojure.test.check.impl.get_current_time_millis = (function clojure$test$check$impl$get_current_time_millis(){
-return (new Date()).valueOf();
-});
